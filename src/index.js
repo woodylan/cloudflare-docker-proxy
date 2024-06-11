5b901a8ef0211cf3b830017467d897c56e93c938
woodylan <woodylan@126.com>
2024-06-11 15:54:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.hkdylan.workers.dev/": "https://registry-1.docker.io",
   "docker.sanqiushu.com": "https://registry-1.docker.io",
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "quay.sanqiushu.com": "https://quay.io",
+  "gcr.sanqiushu.com": "https://k8s.gcr.io",
+  "k8s-gcr.sanqiushu.com": "https://k8s.gcr.io",
+  "ghcr.sanqiushu.com": "https://ghcr.io",
 };
 
 function routeByHosts(host) {
